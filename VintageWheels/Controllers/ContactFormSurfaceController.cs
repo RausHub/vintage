13c8e2649fdc17f7c3510c04617d057f40143281
RausHub <rausjelle@hotmail.com>
2018-04-09 02:28:56 +0200

    captcha

--- VintageWheels/Controllers/ContactFormSurfaceController.cs
+++ VintageWheels/Controllers/ContactFormSurfaceController.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
 using System.Net.Mail;
 using System.Web.Mvc;
 using Umbraco.Web.Mvc;
@@ -24,42 +26,88 @@ namespace VintageWheels.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult HandleContactForm(ContactFormViewModel model)
         {
+            //Check if hidden field was filled in 
             if (!string.IsNullOrEmpty(model.Tip))
             {
                 return CurrentUmbracoPage();
             }
 
+            //Check if captcha was good
+            var googleToken = Request["g-recaptcha-response"];
+            var valid = ValidateCaptcha(googleToken);
+
             //Check if the dat posted is valid (All required's & email set in email field)
-            if (!ModelState.IsValid)
+            if (!ModelState.IsValid && valid)
             {
                 //Not valid - so lets return the user back to the view with the data they entered still prepopulated
                 return CurrentUmbracoPage();
             }
-
-            //Generate an email message object to send
-            MailMessage email = new MailMessage(model.Email, "info@vintage-wheels.be");            
-            email.Subject = string.IsNullOrEmpty(model.Subject) ? "Contact Form Request" : model.Subject;
-            email.Body = model.Message + Environment.NewLine + model.Name;
-
-            try
+            
+            using (SmtpClient smtpClient = new SmtpClient("127.0.0.1", 25))
             {
-                //Connect to SMTP credentials set in web.config
-                SmtpClient smtp = new SmtpClient();
 
-                //Try & send the email with the SMTP settings
-                smtp.Send(email);
-            }
-            catch (Exception ex)
-            {
-                //Throw an exception if there is a problem sending the email
-                throw ex;
+                smtpClient.Credentials = new System.Net.NetworkCredential("Mailer@vintage-wheels.be", "M@1lerJ3113M@art3n");
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtpClient.EnableSsl = false;
+
+                MailMessage email = new MailMessage
+                {
+                    Body = "<p>from: " + model.Name + "</p>" + "<p>email: " + model.Email + "</p>" + "<p>Subject: " + model.Subject + "</p>" + "<p>Message: " + model.Message + "</p>",
+                    IsBodyHtml = true,
+                    Subject = model.Subject
+                };
+                //Setting From , To and CC
+                email.From = new MailAddress("site@vintage-wheels.be");
+                email.To.Add(new MailAddress("rausjelle@hotmail.com"));
+
+
+                try
+                {
+                    //Try & send the email with the SMTP settings
+                    smtpClient.Send(email);
+                }
+                catch (Exception ex)
+                {
+                    //Throw an exception if there is a problem sending the email
+                    throw ex;
+                }
             }
 
             //Update success flag (in a TempData key)
             TempData["IsSuccessful"] = true;
 
             //All done - lets redirect to the current page & show our thanks/success message
-            return RedirectToCurrentUmbracoPage();
+            return CurrentUmbracoPage();
+        }
+
+        private bool ValidateCaptcha(string googleToken)
+        {
+            // new { secret = "", response = googleToken }
+            HttpClient client = new HttpClient();
+            var secret = "6Le541EUAAAAAG0jUZp9Zb8d2IkjXkQeRAEB91_N";
+
+            //LogErrorsAndTransaction("secret: " + secret + " | token: " + googleToken);
+            client.BaseAddress = new Uri("https://www.google.com/recaptcha/api/");
+            //Secret key gebruikt 
+
+            var body = new List<KeyValuePair<string, string>>{
+                new KeyValuePair<string, string>("secret", secret),
+                new KeyValuePair<string, string>("response", googleToken)
+            };
+            var reply = client.PostAsync("siteverify", new FormUrlEncodedContent(body)).Result.Content.ReadAsStringAsync().Result;
+            if (reply.ToLower().Contains("false"))
+            {
+                //LogErrorsAndTransaction(reply);
+                return false;
+
+            }
+            else
+            {
+                //LogErrorsAndTransaction(reply);
+                return true;
+            }
+
         }
     }
 }
